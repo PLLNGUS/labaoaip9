b556cd9c4d3fcd0b95f7898b00cb2965982c167e
Elnar4233KIT <106557105+PLLNGUS@users.noreply.github.com>
2023-04-03 20:02:25 +0300

    Реализовал классы Operator, Operand, OperatorMethod, OperatorContainer

--- labaoaip9/Form1.cs
+++ labaoaip9/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace labaoaip9
 {
@@ -15,6 +16,12 @@ namespace labaoaip9
 
         private Stack<Operator> operators = new Stack<Operator>();
         private Stack<Operand> operands = new Stack<Operand>();
+        private int ConvertCharToInt(object bruh)
+        { 
+            return Convert.ToInt32(bruh.ToString());
+            
+        }
+        
         private bool IsNotOperation(char item)
         {
             if (!(item == 'R' || item == 'M' || item == 'E' || item == 'C' || item == 'S' || item == ',' || item == '(' || item == ')'))
@@ -38,7 +45,7 @@ namespace labaoaip9
         {
 
         }
-
+        bool flag;
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             for (int i = 0; i < textBox1.Text.Length; i++)
@@ -49,6 +56,36 @@ namespace labaoaip9
                     {
                         this.operands.Push(new Operand(textBox1.Text[i]));
                         continue;
+                    }
+                    else if (Char.IsDigit(textBox1.Text[i]))
+                    {
+                        if (Char.IsDigit(textBox1.Text[i + 1]))
+                        {
+                            if (flag)
+                            {
+                                this.operands.Push(new Operand(textBox1.Text[i]));
+                            }
+                            this.operands.Push(new Operand(ConvertCharToInt(this.operands.Pop().value) * 10 + ConvertCharToInt(textBox1.Text[i + 1])));
+                            flag = false;
+                            continue;
+                        }
+                        else if ((textBox1.Text[i + 1] == ','
+                        || textBox1.Text[i + 1] == ')')
+                        && !(Char.IsDigit(textBox1.Text[i - 1])))
+                        {
+                            this.operands.Push(new Operand(ConvertCharToInt
+                            (textBox1.Text[i])));
+                            continue;
+                        }
+                        else if (textBox1.Text[i] == 'R')
+                        {
+                            if (this.operators.Count == 0)
+                            {
+                                this.operators.Push(OperatorContainer.FindOperator
+                                (textBox1.Text[i]));
+                            }
+                        }
+
                     }
 
 
